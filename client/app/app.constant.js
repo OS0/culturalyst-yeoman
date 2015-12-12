c014b120fa799a33ad63a712c1f7fa0be17e4f3a
Samuel Bowler <samuel.bowler@gmail.com>
2015-12-12 17:35:39 -0600

    feat/featured_artists: ng-repeat, buttons

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('culturalystApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -16,6 +16,32 @@ class MainController {
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('thing');
     });
+
+    $scope.featured = [{
+        name: 'Sam Bowler',
+        medium: 'Sculpture',
+        img:'cover_photo_1.png'
+      }, {
+        name: 'Mykia Smith', 
+        medium: 'Writing',
+        img:'cover_photo_2.png'
+      },{
+        name: 'Victor York-Carter', 
+        medium: 'Sex Robots',
+        img:'cover_photo_3.png'
+      },{
+        name: 'Victor York-Carter', 
+        medium: 'Sex Robots',
+        img:'cover_photo_4.png'
+      },{
+        name: 'Victor York-Carter', 
+        medium: 'Sex Robots',
+        img:'cover_photo_5.png'
+      }, {
+        name: 'Victor York-Carter', 
+        medium: 'Sex Robots',
+        img:'cover_photo_6.png'
+      }];
   }
 
   addThing() {
