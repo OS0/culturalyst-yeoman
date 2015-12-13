285876c30e67101489c0b5f1fe9d6dc840a73138
Brian Kustra <bkustrakb@gmail.com>
2015-12-12 18:37:38 -0600

    Forgot to start new branch

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -20,5 +20,43 @@ angular.module('culturalystApp')
     		},200);
     	}
     };
+
+    $scope.featured = [{
+        name: 'Sam Bowler',
+        medium: 'Sculpture',
+        img:'cover_photo_1.png'
+      }, {
+        name: 'Mykia Smith', 
+        medium: 'Writing',
+        img:'cover_photo_2.png'
+      },{
+        name: 'Just Some Dude', 
+        medium: 'Musician',
+        img:'cover_photo_3.png'
+      },{
+        name: 'Victor York-Carter', 
+        medium: 'Musician',
+        img:'cover_photo_4.png'
+      },{
+        name: 'Just Some Dude', 
+        medium: 'Musician',
+        img:'cover_photo_5.png'
+      }, {
+        name: 'Victor York-Carter', 
+        medium: 'Musician',
+        img:'cover_photo_6.png'
+      },{
+        name: 'Just Some Dude', 
+        medium: 'Musician',
+        img:'cover_photo_1.png'
+      },{
+        name: 'Victor York-Carter', 
+        medium: 'Musician',
+        img:'cover_photo_2.png'
+      },{
+        name: 'Just Some Dude', 
+        medium: 'Musician',
+        img:'cover_photo_3.png'
+      }];
   });
   
