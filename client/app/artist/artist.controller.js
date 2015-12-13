052b9126d53932f05d2a56b989366dd88cf94a11
rascle <ryan.bascle@gmail.com>
2015-12-12 19:43:22 -0600

    More uncaught changes in artist.

--- client/app/artist/artist.controller.js
+++ client/app/artist/artist.controller.js
@@ -1,12 +1,6 @@
 'use strict';
 
 angular.module('culturalystApp')
-<<<<<<< HEAD
   .controller('ArtistCtrl', ['$state', function($state){
     $state.go('artist.bio')
-  }])
-=======
-  .controller('ArtistCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
->>>>>>> Squash before rebase
+  }]);
