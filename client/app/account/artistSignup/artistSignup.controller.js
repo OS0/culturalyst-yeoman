7e7cd99e6125d2b4d7460ae03a60ed4dc974c035
Victor York <victor.a.york@gmail.com>
2015-12-15 13:39:14 -0600

    Attempt to update user data in the db on the second signup page.

--- client/app/account/artistSignup/artistSignup.controller.js
+++ client/app/account/artistSignup/artistSignup.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
+class ArtistSignupController {
+  constructor(Auth, $state, $log, $scope, $http) {
+    this.Auth = Auth;
+    this.$state = $state;
+    this.$log = $log;
+    this.$scope = $scope;
+    this.$http = $http;
+  }
+
+}
+
 angular.module('culturalystApp')
-  .controller('ArtistSignupCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('ArtistSignupController', ArtistSignupController);
