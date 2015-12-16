36d2d81f339a3271a40f268b94b7a18f7e4e1a11
Victor York <victor.a.york@gmail.com>
2015-12-15 21:11:11 -0600

    Attempt to allow users to update profile information.

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -16,7 +16,7 @@ class SignupController {
   }
 
   currentUser() {
-    this.$log.info(this.Auth.getCurrentUser());
+    //this.$log.info(this.Auth.getCurrentUser());
     return this.Auth.getCurrentUser;
   }
 
@@ -51,93 +51,33 @@ class SignupController {
   }
 
   // edit this to work on the next page
-  userDetail() {
-    /* TODO: HTTP put request maybe */
-    currentUser()().location = this.user.location;
-    currentUser()().birthday = this.user.birthday;
-    // Account updated, redirect to home
-    /* TODO: Change the state if possable */
-    this.$state.go('discovery');
-    //.catch(err => {
-    //  err = err.data;
-    //  this.errors = {};
-    //
-    //  // Update validity of form fields that match the sequelize errors
-    //  if (err.name) {
-    //    angular.forEach(err.fields, field => {
-    //      form[field].$setValidity('mongoose', false);
-    //      this.errors[field] = err.message;
-    //    });
-    //  }
-    //});
+  userDetail(form) {
+    this.submitted = true;
+
+    if (form.$valid) {
+      this.Auth.updateUserInfo({
+          name: this.user.name,
+          location: this.user.location,
+          birthday: this.user.birthday
+        })
+        .then(() => {
+          this.state.go('discovery')
+        })
+        .catch(err => {
+          err = err.data;
+          this.errors = {};
+
+          /*// Update validity of form fields that match the sequelize errors
+          if (err.name) {
+            angular.forEach(err.fields, field => {
+              form[field].$setValidity('mongoose', false);
+              this.errors[field] = err.message;
+            });
+          }*/
+        });
+    }
   }
 }
 
 angular.module('culturalystApp')
   .controller('SignupController', SignupController);
-
-/*
- 'use strict';
-
- class SignupController {
- //start-non-standard
- user = {};
- errors = {};
- submitted = false;
- //end-non-standard
-
- constructor(Auth, $state, $log, $scope, $http) {
- this.Auth = Auth;
- this.$state = $state;
- this.$log = $log;
- this.$scope = $scope;
- this.$http = $http;
-
- this.$log.info(this.Auth.getCurrentUser()._id);
-
- this.$scope.user = this.Auth.getCurrentUser();
-
- this.$log.info(this.$scope.name);
- }
-
- register(form) {
- this.submitted = true;
-
- if (form.$valid) {
- this.Auth.createUser({
- profileImage: null,
- name: this.user.name,
- email: this.user.email,
- password: this.user.password,
- location: null
- })
- .then(() => {
- // Account created, redirect to signupInfo for detailed signup page
- // view
- this.$state.go('signupInfo');
- })
- .catch(err => {
- err = err.data;
- this.errors = {};
-
- // Update validity of form fields that match the sequelize errors
- if (err.name) {
- angular.forEach(err.fields, field => {
- form[field].$setValidity('mongoose', false);
- this.errors[field] = err.message;
- });
- }
- });
- }
- }
-
- completeSignup(form) {
- //submit will save the form to the database and update push the user to
- // the discovery page
- //this.$state.go('discovery');
- }
- }
-
- angular.module('culturalystApp')
- .controller('SignupController', SignupController);
- */
--- server/config/seed.js
+++ server/config/seed.js
@@ -9,24 +9,38 @@ var User = sqldb.User;
 
 User.sync()
   .then(function() {
-    return User.destroy({ where: {} });
+    return User.destroy({where: {}});
   })
   .then(function() {
     User.bulkCreate([{
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test',
-      profilePicture: null
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin',
-      profilePicture: null
-    }])
-    .then(function() {
-      console.log('finished populating users');
-    });
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'test',
+        profilePicture: null
+      }, {
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: 'admin',
+        profilePicture: null
+      }, {
+        provider: 'local',
+        role: 'catalyst',
+        name: 'Catalyst',
+        email: 'catalyst@example.com',
+        password: 'catalyst',
+        profilePicture: null
+      }, {
+        provider: 'local',
+        role: 'creative',
+        name: 'Creative',
+        email: 'creative@example.com',
+        password: 'creative',
+        profilePicture: null
+      }])
+      .then(function() {
+        console.log('finished populating users');
+      });
   });
