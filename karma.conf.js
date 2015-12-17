5e7d6f8c68c78f3de55a60b35bbf85411532dc2a
ahrobinson <ahrobins3@gmail.com>
2015-12-17 13:18:11 -0600

    feat/firebase modularize firebase code with firebase factory
    feat/firebase get and display indiv. artist info, post messages by indiv.
    
    add some style

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,9 @@ module.exports = function(config) {
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
       'client/bower_components/angular-validation-match/dist/angular-validation-match.min.js',
+      'client/bower_components/moment/moment.js',
+      'client/bower_components/firebase/firebase.js',
+      'client/bower_components/angularfire/dist/angularfire.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
