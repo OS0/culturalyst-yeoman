311a27860f656362c8540bc73462b14d62396db6
ahrobinson <ahrobins3@gmail.com>
2015-12-16 21:26:18 -0600

    feat/artist/firebase add some style, get data from firebase

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -1,65 +1,72 @@
 'use strict';
 
 angular.module('culturalystApp')
-  .controller('DiscoveryCtrl', function ($scope, $http, MediaList) {
+.controller('DiscoveryCtrl', function($scope, $firebaseArray,$firebaseObject, MediaList){
+  var ref = new Firebase('https://culturalyst.firebaseio.com/Users')
+  $scope.mediaList = MediaList.getMediaList();
+  console.log('hi')
+  $scope.artists= $firebaseArray(ref)
 
-    $scope.mediaList = MediaList.getMediaList();
-    $scope.artists = [];
+})
 
-    $scope.loadSubMediums = function(medium){
-      console.log(medium.name)
-      $scope.selectedMedium = medium.name;
-      $scope.submedia = medium.submedia;
-    };
-
-    $scope.getArtists = function() {
-      $http.get('/api/users/discovery/' + $scope.selectedMedium + '/' + $scope.selectedSubmedium).then(function(response) {
-        $scope.artists = response.data;
-      })
-    }
-    
-
-    $scope.featured = [{
-        name: 'Sam Bowler',
-        medium: 'Sculpture',
-        img:'cover_photo_1.png'
-      }, {
-        name: 'Mykia Smith', 
-        medium: 'Writing',
-        img:'cover_photo_2.png'
-      },{
-        name: 'Just Some Dude', 
-        medium: 'Musician',
-        img:'cover_photo_3.png'
-      },{
-        name: 'Victor York-Carter', 
-        medium: 'Musician',
-        img:'cover_photo_4.png'
-      },{
-        name: 'Just Some Dude', 
-        medium: 'Musician',
-        img:'cover_photo_5.png'
-      }, {
-        name: 'Victor York-Carter', 
-        medium: 'Musician',
-        img:'cover_photo_6.png'
-      },{
-        name: 'Just Some Dude', 
-        medium: 'Musician',
-        img:'cover_photo_1.png'
-      },{
-        name: 'Victor York-Carter', 
-        medium: 'Musician',
-        img:'cover_photo_2.png'
-      },{
-        name: 'Just Some Dude', 
-        medium: 'Musician',
-        img:'cover_photo_3.png'
-      }];
-  })
+  // .controller('DiscoveryCtrl', function ($scope, $http, MediaList) {
+  //
+  //   $scope.mediaList = MediaList.getMediaList();
+  //   $scope.artists = [];
+  //
+  //   $scope.loadSubMediums = function(medium){
+  //     console.log(medium.name)
+  //     $scope.selectedMedium = medium.name;
+  //     $scope.submedia = medium.submedia;
+  //   };
+  //
+  //   $scope.getArtists = function() {
+  //     $http.get('/api/users/discovery/' + $scope.selectedMedium + '/' + $scope.selectedSubmedium).then(function(response) {
+  //       $scope.artists = response.data;
+  //     })
+  //   }
+  //
+  //
+  //   $scope.featured = [{
+  //       name: 'Sam Bowler',
+  //       medium: 'Sculpture',
+  //       img:'cover_photo_1.png'
+  //     }, {
+  //       name: 'Mykia Smith',
+  //       medium: 'Writing',
+  //       img:'cover_photo_2.png'
+  //     },{
+  //       name: 'Just Some Dude',
+  //       medium: 'Musician',
+  //       img:'cover_photo_3.png'
+  //     },{
+  //       name: 'Victor York-Carter',
+  //       medium: 'Musician',
+  //       img:'cover_photo_4.png'
+  //     },{
+  //       name: 'Just Some Dude',
+  //       medium: 'Musician',
+  //       img:'cover_photo_5.png'
+  //     }, {
+  //       name: 'Victor York-Carter',
+  //       medium: 'Musician',
+  //       img:'cover_photo_6.png'
+  //     },{
+  //       name: 'Just Some Dude',
+  //       medium: 'Musician',
+  //       img:'cover_photo_1.png'
+  //     },{
+  //       name: 'Victor York-Carter',
+  //       medium: 'Musician',
+  //       img:'cover_photo_2.png'
+  //     },{
+  //       name: 'Just Some Dude',
+  //       medium: 'Musician',
+  //       img:'cover_photo_3.png'
+  //     }];
+  // })
   .controller('SearcherCtrl', function ($scope) {
     $scope.currentSel = function() {
       console.log($scope.selectedMedium);
     }
   });
-  
