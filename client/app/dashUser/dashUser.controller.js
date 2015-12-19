14a309e02dabdecd65c4725b31734da72f6f1e3b
Alice Green <green.alice.e@gmail.com>
2015-12-18 19:31:58 -0600

    Created rewards view in the UserDashboard

--- client/app/dashUser/dashUser.controller.js
+++ client/app/dashUser/dashUser.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('culturalystApp')
-  .controller('DashUserCtrl', function ($scope, $log, Auth) {
+  .controller('DashUserCtrl', function ($scope, $log, Auth, $http) {
     $scope.message = 'Hello';
     $log.log(Auth);
-    //constructor(Auth) {
-    //  this.getCurrentUser = Auth.getCurrentUser;
-    //}
-    $scope.currentUser = Auth.getCurrentUser.name;
+    $http.get('/api/users/me').then(function(res) {
+      $scope.me = res.data;
+      $log.log($scope.me);
+    });
   });
--- client/app/dashUser/dashUser.js
+++ client/app/dashUser/dashUser.js
@@ -7,5 +7,14 @@ angular.module('culturalystApp')
         url: '/dashUser',
         templateUrl: 'app/dashUser/dashUser.html',
         controller: 'DashUserCtrl'
+      })
+      .state('dashUser.rewards', {
+        templateUrl: 'app/dashUser/dashUser.rewards.html'
+      })
+      .state('dashUser.messages', {
+        templateUrl: 'app/dashUser/dashUser.messages.html'
+      })
+      .state('dashUser.mediums', {
+        templateUrl: 'app/dashUser/dashUser.mediums.html'
       });
   });
