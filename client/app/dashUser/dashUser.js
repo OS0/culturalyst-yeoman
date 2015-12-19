6246925575dec2a876dfc4dbf141b00fab6a19a0
Alice Green <green.alice.e@gmail.com>
2015-12-19 13:49:19 -0600

    Squashing in order to push up working user dashboard
    Cleaned up User Dashboard view - SQUASH Here
    
    User Dashboard with Side Nav
    
    Created rewards view in the UserDashboard
    
    Update user dash views
    
    Updated initial view in user dash

--- client/app/dashUser/dashUser.js
+++ client/app/dashUser/dashUser.js
@@ -7,5 +7,20 @@ angular.module('culturalystApp')
         url: '/dashUser',
         templateUrl: 'app/dashUser/dashUser.html',
         controller: 'DashUserCtrl'
+      })
+      .state('dashUser.myList', {
+        templateUrl: 'app/dashUser/dashUser.myList.html'
+      })
+      .state('dashUser.rewards', {
+        templateUrl: 'app/dashUser/dashUser.rewards.html'
+      })
+      .state('dashUser.messages', {
+        templateUrl: 'app/dashUser/dashUser.messages.html'
+      })
+      .state('dashUser.mediums', {
+        templateUrl: 'app/dashUser/dashUser.mediums.html'
+      })
+      .state('dashUser.profile', {
+        templateUrl: 'app/dashUser/dashUser.profile.html'
       });
   });
