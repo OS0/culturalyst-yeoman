a1877a1012b6a1db350eeffb423d371aca7ba065
Alice Green <green.alice.e@gmail.com>
2015-12-18 18:04:57 -0600

    User Dashboard with Side Nav

--- client/app/dashUser/dashUser.controller.js
+++ client/app/dashUser/dashUser.controller.js
@@ -4,4 +4,8 @@ angular.module('culturalystApp')
   .controller('DashUserCtrl', function ($scope, $log, Auth) {
     $scope.message = 'Hello';
     $log.log(Auth);
+    //constructor(Auth) {
+    //  this.getCurrentUser = Auth.getCurrentUser;
+    //}
+    $scope.currentUser = Auth.getCurrentUser.name;
   });
