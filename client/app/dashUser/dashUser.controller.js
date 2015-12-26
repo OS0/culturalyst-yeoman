ee703b5ea9e5943cd53215f9a9c912b976412cda
Brian Kustra <bkustrakb@gmail.com>
2015-12-26 12:58:54 -0600

    Minor refactoring and outline for POST/PUTing to DB

--- client/app/dashUser/dashUser.controller.js
+++ client/app/dashUser/dashUser.controller.js
@@ -22,7 +22,37 @@ angular.module('culturalystApp')
       code: false,
       gaming: false
     };
+
     $scope.toggleMedium = function(medium) {
       $scope.mediums[medium] = $scope.mediums[medium] === false ? true : false;
+    }
+
+    $scope.favorites = [
+      {
+        img: "data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+        name: 'Alon',
+        title: 'Rapper Extraordinaire'
+      },
+      {
+        img: "data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+        name: 'Sam',
+        title: 'Sculptor'
+      },
+      {
+        img: "data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+        name: 'Brian',
+        title: 'Philosopher'
+      },
+      {
+        img: "data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+        name: 'Mykia',
+        title: 'Token Girl'
+      }
+    ];
+
+    $scope.saveToDB = function() {
+      // check mediums table for true values (on DOM?)
+      // add trues values to something like
+        // api/users/mediums
     }
   });
