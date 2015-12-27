3421622bb0a4902dfc008a8236beb928ae57cb91
Samuel Bowler <samuel.bowler@gmail.com>
2015-12-27 12:20:08 -0600

    Userinfo as new controller and route. Update database once, not working on a second call. Need to fix the query in the user controller call to updateUserInfo.
    Save state before pairing with ryan.
    
    Save state
    
    implement basic artist registration.
    
    User info updates properly. Remove birthday updating.
    
    Added api route for handler updateArtistInfo.
    Added reward string to user model for placeholder.
    
    Artist info updating. Pulling artists from database to profile.
    
    Update userInfo & artistInfo working.
    Profile page querying users and return info.
    And the world cried.
    
    Merge pull request #28 from Rascle/rebase
    
    Update userInfo & artistInfo working. Profile page querying users and return info. And the world cried.
    Squashing in order to push up working user dashboard
    
    Cleaned up User Dashboard view - SQUASH Here
    
    User Dashboard with Side Nav
    
    Created rewards view in the UserDashboard
    
    Update user dash views
    
    Updated initial view in user dash
    
    Added User Profile Glyph to Navbar and Updated Medium Buttons in the User Dash
    
    Added sidebar
    
    Merge pull request #29 from aegreen/feat/userDash
    
    Feat/user dash
    Update footer, add about link to navbar
    
    Add about page
    
    Sidebar placement
    
    Merge pull request #30 from mykia/feat/about
    
    About page and footer
    feat/SQLREBASE: committing before rebase
    
    contentUpload: working commit
    
    feat/upload - working upload

--- client/app/artist/artist.controller.js
+++ client/app/artist/artist.controller.js
@@ -1,34 +1,95 @@
 'use strict';
 
 angular.module('culturalystApp')
-  .controller('ArtistCtrl', function($scope, $location, $http){
-    var artistId = $location.path().split('/').pop();
+  .controller('ArtistCtrl', ['$scope', '$rootScope','$location', 'Upload', '$http',
+    function($scope, $rootScope, $location, $upload, $http) {
 
-    $http.get('/api/users/' + artistId).then(function(res) {
+    $scope.artistId = $location.path().split('/').pop();
+    $scope.posts = [];
+    $scope.postId;
+    $scope.photo = false;
+    // console.log(artistId)
+    // Artist(artistId).then(function(artist){
+    //   $scope.artistData = artist;
+    //
+
+    $http.get('/api/users/' + $scope.artistId).then(function(res) {
       console.log(res);
       $scope.artist = res.data;
     })
+
+    $http.get('/api/content/' + $scope.artistId).then(function(res) {
+      console.log(res);
+      $scope.content = res.data;
+    })
+
+
+     $scope.saveContent = function(post){
+      if ($scope.photo){
+        $http.put('/api/content/' + $scope.artistId, {_id: $scope.postId, info: post}).then(function(response){
+            console.log(response.data);
+            $scope.posts.push(response.data);
+            $scope.photo = false;
+        })
+      } else {
+        $http.post('/api/content/' + $scope.artistId, {_id: $scope.postId, info: post, type: "post"}).then(function(response){
+            console.log(response.data);
+            $scope.posts.push(response.data);
+        })
+      }
+    };
+
+    $scope.saveContentPhoto = function(url){
+      console.log('this was fired');
+        $http.post('/api/content/' + $scope.artistId, {_id: $scope.postId,url: url, type:"post"}).then(function(response){
+            console.log(response.data);
+            $scope.posts.push(response.data);
+            $scope.postId = response.data._id;
+        })
+    };
+
+    $scope.getArtistContent = function(){
+      console.log('this fired');
+      console.log($scope.artistId);
+      $http.get('/api/content/' + $scope.artistId +'/getContent').then(function(response){
+        console.log(response.data);
+        response.data.forEach(function(info){
+          $scope.posts.push(info);
+        });
+        console.log('content: ', response);
+      })
+    };
+
+    $scope.uploadFiles = function(files){
+      $scope.files = files;
+      if (!$scope.files) return;
+      angular.forEach(files, function(file){
+        if (file && !file.$error) {
+          file.upload = $upload.upload({
+            url: "https://api.cloudinary.com/v1_1/" + $.cloudinary.config().cloud_name + "/upload",
+            fields: {
+              upload_preset: $.cloudinary.config().upload_preset,
+              tags: 'myphotoalbum',
+              context: 'photo=' + $scope.title
+            },
+            file: file
+          }).progress(function (e) {
+            file.progress = Math.round((e.loaded * 100.0) / e.total);
+            file.status = "Uploading... " + file.progress + "%";
+          }).success(function (data, status, headers, config) {
+            $rootScope.photos = $rootScope.photos || [];
+            data.context = {custom: {photo: $scope.title}};
+            file.result = data;
+            $rootScope.photos.push(data);
+            $scope.photo = true;
+            $scope.saveContentPhoto(data.secure_url);
+            console.log($scope.photo);
+          }).error(function (data, status, headers, config) {
+            file.result = data;
+          });
+        }
+      });
+    };
     
-  });
+  }]);
 
-
-    // $firebaseObject(cultFire.child('Users').child('/' + artistId))
-    //   .$loaded()
-    //   .then(function(data){
-    //   $scope.artist = data
-    //   console.log($scope.artist)
-    // })
-
-    // $scope.posts = $firebaseArray(cultFire.child('posts').child('/' + artistId))
-
-    // console.log($scope.postsy)
-    // $scope.addPost = function(message) {
-    //   console.log($scope.posts)
-    //   // console.log($scope.postsId)
-    //   $scope.posts.$add({
-    //     name: $scope.artist.name,
-    //     text: message,
-    //     created: Firebase.ServerValue.TIMESTAMP,
-    //     id: artistId
-    //   });
-    // };
