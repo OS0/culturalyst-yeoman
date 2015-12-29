777c02ac56b4be34e0407c9b5e5542e57c6c9cfa
Alice Green <green.alice.e@gmail.com>
2015-12-28 21:10:49 -0600

    Added more media and submedia to the media list

--- client/app/discovery/mediaList/mediaList.service.js
+++ client/app/discovery/mediaList/mediaList.service.js
@@ -5,9 +5,22 @@ angular.module('culturalystApp')
     // Service logic
 
     var mediaList = [
-      {'name': 'Music', 'submedia': ['Hip-hop', 'Guitar', 'Brass', 'Folk', 'Rock','Country']},
-      {'name': 'Performing Arts', 'submedia': ['Dance', 'Theater', 'Ballet']}
-    ]
+      {'name': 'Animation', 'submedia': ['Claymation', 'Drawn Animation', 'Stop Animation']},
+      {'name': 'Comedy'},
+      {'name': 'Comics', 'submedia': ['Anime', 'Graphic Novels']},
+      {'name': 'Crafts and DIY', 'submedia': ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts']},
+      {'name': 'Dance and Theater', 'submedia': ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical','Theater', 'Plays', 'Musicals']},
+      {'name': 'Education'},
+      {'name': 'Games'},
+      {'name': 'Music', 'submedia': ['Brass', 'Classical', 'Country', 'Experimental', 'Folk', 'Hip-hop', 'Jazz', 'Rock']},
+      {'name': 'Painting and Drawing', 'submedia': ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil']},
+      {'name': 'Photography', 'submedia': ['Digital', 'Film']},
+      {'name': 'Podcasts'},
+      {'name': 'Science'},
+      {'name': 'Sculpture', 'submedia': ['Clay', 'Metal', 'Wood']},
+      {'name': 'Video and Film', 'submedia': ['Documentaries', 'Feature Films', 'Short Films']},
+      {'name': 'Writing', 'submedia': ['Fiction', 'Non-Fiction', 'Poetry']}
+    ];
 
     // Public API here
     return {
