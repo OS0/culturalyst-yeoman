83f7bed79a691e27699ecfebb4bc93606c44be1c
Brian Kustra <bkustrakb@gmail.com>
2015-12-30 16:00:25 -0600

    Routes working, media list altered for queries

--- client/app/discovery/discovery.js
+++ client/app/discovery/discovery.js
@@ -8,9 +8,64 @@ angular.module('culturalystApp')
         templateUrl: 'app/discovery/discovery.html',
         controller: 'DiscoveryCtrl'
       })
+      .state('music', {
+        url: '/discovery/Music',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('writing', {
+        url: '/discovery/Writing',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('film', {
+        url: '/discovery/Film',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('visual art', {
+        url: '/discovery/Visual',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('photography', {
+        url: '/discovery/Photography',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
       .state('dance', {
-        url: '/discovery/dance',
-        templateUrl: 'app/discovery/mediums/dancer.html',
+        url: '/discovery/Dance',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('culture bearers', {
+        url: '/discovery/Bearers',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('comedy', {
+        url: '/discovery/Comedy',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('crafts and dyi', {
+        url: '/discovery/Crafts',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('design', {
+        url: '/discovery/Design',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('code', {
+        url: '/discovery/Code',
+        templateUrl: 'app/discovery/discovery.html',
+        controller: 'DiscoveryCtrl',
+      })
+      .state('sculpture', {
+        url: '/discovery/Sculpture',
+        templateUrl: 'app/discovery/discovery.html',
         controller: 'DiscoveryCtrl',
       })
   });
--- client/app/discovery/mediaList/mediaList.service.js
+++ client/app/discovery/mediaList/mediaList.service.js
@@ -4,25 +4,39 @@ angular.module('culturalystApp')
   .factory('MediaList', function () {
     // Service logic
 
-    // Expanded mediaList arranged alphabetically
     var mediaList = [
-      {'name': 'Animation', 'submedia': ['Claymation', 'Drawn Animation', 'Stop Animation']},
-      {'name': 'Comedy'},
-      {'name': 'Comics', 'submedia': ['Anime', 'Graphic Novels']},
-      {'name': 'Crafts and DIY', 'submedia': ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts']},
-      {'name': 'Culture Bearers', 'submedia': ['Mardi Gras Indians', 'Social Aid and Pleasure Clubs']},
-      {'name': 'Dance and Theater', 'submedia': ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical','Theater', 'Plays', 'Musicals']},
-      {'name': 'Education'},
-      {'name': 'Games'},
       {'name': 'Music', 'submedia': ['Brass', 'Classical', 'Country', 'Experimental', 'Folk', 'Hip-hop', 'Jazz', 'Rock']},
-      {'name': 'Painting and Drawing', 'submedia': ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil']},
+      {'name': 'Writing', 'submedia': ['Fiction', 'Non-Fiction', 'Poetry']},
+      {'name': 'Film', 'submedia': ['Documentaries', 'Feature Films', 'Short Films']},
+      {'name': 'Visual', 'submedia': ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil']},
       {'name': 'Photography', 'submedia': ['Digital', 'Film']},
-      {'name': 'Podcasts'},
-      {'name': 'Science'},
+      {'name': 'Dance', 'submedia': ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical','Theater', 'Plays', 'Musicals']},
+      {'name': 'Bearers', 'submedia': ['Mardi Gras Indians']},
+      {'name': 'Comedy', 'submedia': ['Dark', 'Upbeat']},
+      {'name': 'Crafts', 'submedia': ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts']},
+      {'name': 'Design'},
+      {'name': 'Code', 'submedia': ['Front-end', 'Back-end', 'Full-stack']},
       {'name': 'Sculpture', 'submedia': ['Clay', 'Metal', 'Wood']},
-      {'name': 'Video and Film', 'submedia': ['Documentaries', 'Feature Films', 'Short Films']},
-      {'name': 'Writing', 'submedia': ['Fiction', 'Non-Fiction', 'Poetry']}
-    ];
+    ]
+    // Expanded mediaList arranged alphabetically
+    // var mediaList = [
+    //   {'name': 'Animation', 'submedia': ['Claymation', 'Drawn Animation', 'Stop Animation']},
+    //   {'name': 'Comedy'},
+    //   {'name': 'Comics', 'submedia': ['Anime', 'Graphic Novels']},
+    //   {'name': 'Crafts and DIY', 'submedia': ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts']},
+    //   {'name': 'Culture Bearers', 'submedia': ['Mardi Gras Indians', 'Social Aid and Pleasure Clubs']},
+    //   {'name': 'Dance and Theater', 'submedia': ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical','Theater', 'Plays', 'Musicals']},
+    //   {'name': 'Education'},
+    //   {'name': 'Games'},
+    //   {'name': 'Music', 'submedia': ['Brass', 'Classical', 'Country', 'Experimental', 'Folk', 'Hip-hop', 'Jazz', 'Rock']},
+    //   {'name': 'Painting and Drawing', 'submedia': ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil']},
+    //   {'name': 'Photography', 'submedia': ['Digital', 'Film']},
+    //   {'name': 'Podcasts'},
+    //   {'name': 'Science'},
+    //   {'name': 'Sculpture', 'submedia': ['Clay', 'Metal', 'Wood']},
+    //   {'name': 'Video and Film', 'submedia': ['Documentaries', 'Feature Films', 'Short Films']},
+    //   {'name': 'Writing', 'submedia': ['Fiction', 'Non-Fiction', 'Poetry']}
+    // ];
 
     // Public API here
     return {
