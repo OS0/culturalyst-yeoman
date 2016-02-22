56c4fd34ac697d5dd2f39f46d52f4d533d8f6611
vicyork <victor.a.york@gmail.com>
2016-02-22 15:17:51 -0600

    Convert the Upload Controller to es 6

--- client/components/uploadContent/config.js
+++ client/components/uploadContent/config.js
@@ -1,6 +1,5 @@
 //console.log($.cloudinary);
-//
-//$.cloudinary.config().cloud_name = 'culturalyst';
+////$.cloudinary.config().cloud_name = 'culturalyst';
+////$.cloudinary.config().upload_preset = 'lebxtfjo';
+//$.cloudinary.config({cloud_name: 'culturalyst', api_key: '521965896796486'});
 //console.log($.cloudinary);
-//
-//$.cloudinary.config().upload_preset = 'lebxtfjo';
